c4f2dd580f28fd6eee374ab4bf37085640ac3ba3
paulsarms <armstrong.pa@gmail.com>
2017-01-04 20:07:05 +0000

    added first arduino sketch to repo

---
+++ Arduino Sensor Sketches/Display_Temp_On_LCD/Display_Temp_On_LCD.ino
@@ -0,0 +1,56 @@
+/* YourDuino.com Example Software Sketch
+ 16 character 2 line I2C Display
+ Backpack Interface labelled "YwRobot Arduino LCM1602 IIC V1"
+ terry@yourduino.com */
+ 
+/*-----( Import needed libraries )-----*/
+#include <Wire.h>  // Comes with Arduino IDE
+// Get the LCD I2C Library here: 
+// https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
+// Move any other LCD libraries to another folder or delete them
+// See Library "Docs" folder for possible commands etc.
+#include <LiquidCrystal_I2C.h>
+ 
+/*-----( Declare Constants )-----*/
+/*-----( Declare objects )-----*/
+// set the LCD address to 0x27 for a 20 chars 4 line display
+// Set the pins on the I2C chip used for LCD connections:
+//                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 7
+void setup()
+{
+  Serial.begin(9600);
+  // initialize library
+  lcd.begin(16,2);
+  // blink backlight three times
+  for(int i = 0; i< 3; i++)
+  {
+    lcd.backlight();
+    delay(250);
+    lcd.noBacklight();
+    delay(250);
+  }
+  lcd.backlight();
+  // set cursor to positon x=0, y=0
+}
+ 
+ 
+void loop()
+{
+    int chk = DHT.read11(DHT11_PIN);
+  String temp = "Temperature = ";
+  String tempval = temp +DHT.temperature;
+  String hum = "Humidity = ";
+  String humval = hum +DHT.humidity;
+  lcd.setCursor(0,0);
+  lcd.print(tempval);
+  lcd.setCursor(0,1);
+  lcd.print(humval);
+  delay(1000);
+}
