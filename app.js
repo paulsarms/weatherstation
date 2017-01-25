a625eaaab3986675453fed43eede1056c94c3ab8
paulsarms <armstrong.pa@gmail.com>
2017-01-25 00:00:40 +0000

    added directory paths

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var moment = require('moment');
 var serialport = require("serialport");
 var SerialPort = serialport;
 var config = require('./config');
-
+var path = require('path');
+var appDir = path.dirname(require.main.filename);
 //public files
 app.use(express.static('public'));
 // Start the Server
@@ -25,8 +26,8 @@ var server = app.listen(8081, function () {
 
 // Make CSV file if it does exist. Use these headings:
 // timestamp, temperature... etc
-if (fs.existsSync('public/data.csv')==false){
-  fs.writeFile('public/data.csv', 'timestamp,dht22_temp,bmp_180_temp,dht22_hum,bmp_180_presure\n', (err) => {
+if (fs.existsSync(appDir + '/public/data.csv')==false){
+  fs.writeFile(appDir + '/public/data.csv', 'timestamp,dht22_temp,bmp_180_temp,dht22_hum,bmp_180_presure\n', (err) => {
   if (err) throw err;
     console.log('CSV file created');
   });
@@ -43,7 +44,7 @@ serialPort.on("open", function () {
     console.log(data);
     // Append new data from serialport to the CSV
     var data=moment().format() + "," + data + "\n";
-    fs.appendFile('public/data.csv', data, (err) => {
+    fs.appendFile(appDir + '/public/data.csv', data, (err) => {
       if(err) {
           return console.log(err);
       }
